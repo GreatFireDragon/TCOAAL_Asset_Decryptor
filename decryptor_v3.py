0b041db1f6161cf091bdd073efd151a1dcfded2b
GreatFireDragon <90414648+GreatFireDragon@users.noreply.github.com>
2025-09-30 14:10:19 +0300

    Update decryptor_v3.py
    Minor fixes to prepare for .exe packing:
    - Added game directory search
    - Added notification to put .exe in game's dir if search fails
    - Credits moved to "Success" notif
    - Added myself to the credits

--- decryptor_v3.py
+++ decryptor_v3.py
@@ -1,88 +1,99 @@
 import sys
 import os
+import string
+import tkinter as tk
+from tkinter import messagebox
 
-file_counter=0
+file_counter = 0
 
 def decryptFolder(folder):
-  global file_counter
+    global file_counter
 
-  folderPath = os.path.join(gamePath, 'www', folder)
-  
-  for file in os.listdir(folderPath):
-    file_counter+=1
-  
-    with open(os.path.join(folderPath, file), 'rb') as rf:
-      data=rf.read()
+    folderPath = os.path.join(gamePath, 'www', folder)
 
-    if True:
-      extension=''
-      topFolder = folder.split('/')[0]
-      
-      if topFolder=='data':
-        extension = '.json'
-      elif topFolder=='audio':
-        extension = '.ogg'
-      elif topFolder=='img':
-        extension = '.png'
-      else:
-        print('Unknown top folder')
-        quit()
+    for file in os.listdir(folderPath):
+        file_counter += 1
 
-      data=decrypt(data, folder + '/' + file)
-      
-      newFile = os.path.basename(file) + extension
-      
-      os.makedirs(os.path.join(outPath, folder), exist_ok=True)
-      
-      with open(os.path.join(outPath, folder, newFile), 'wb') as wf:
-        wf.write(data)
+        with open(os.path.join(folderPath, file), 'rb') as rf:
+            data = rf.read()
 
-    if file_counter%50==0:
-      print(str(file_counter)+' files processed')
+        if True:
+            extension = ''
+            topFolder = folder.split('/')[0]
+
+            if topFolder == 'data':
+                extension = '.json'
+            elif topFolder == 'audio':
+                extension = '.ogg'
+            elif topFolder == 'img':
+                extension = '.png'
+            else:
+                messagebox.showerror("Error", "Unknown top folder: " + topFolder)
+                sys.exit()
+
+            data = decrypt(data, folder + '/' + file)
+
+            newFile = os.path.basename(file) + extension
+
+            os.makedirs(os.path.join(outPath, folder), exist_ok=True)
+
+            with open(os.path.join(outPath, folder, newFile), 'wb') as wf:
+                wf.write(data)
+
 
 def decrypt(raw_bytes, idFilePath):
-  encrypted_data = raw_bytes[len(signature) + 1:]
+    encrypted_data = raw_bytes[len(signature) + 1:]
 
-  encrypt_offset = raw_bytes[len(signature)]
-  if encrypt_offset == 0:
-    encrypt_offset = len(encrypted_data)
+    encrypt_offset = raw_bytes[len(signature)]
+    if encrypt_offset == 0:
+        encrypt_offset = len(encrypted_data)
 
-  mask = (getMask(idFilePath) + 1) % 256
-  decrypted_data = bytearray(encrypted_data)
+    mask = (getMask(idFilePath) + 1) % 256
+    decrypted_data = bytearray(encrypted_data)
 
-  for i in range(encrypt_offset):
-    original_byte = encrypted_data[i]
-    decrypted_data[i] = original_byte ^ mask
-    mask = ((mask << 1) ^ original_byte) & 255
+    for i in range(encrypt_offset):
+        original_byte = encrypted_data[i]
+        decrypted_data[i] = original_byte ^ mask
+        mask = ((mask << 1) ^ original_byte) & 255
 
-  return bytes(decrypted_data)
+    return bytes(decrypted_data)
 
 def getMask(filename):
-  mask = 0
-  filename = filename.split('/')[-1].upper()
-	
-  for char in filename:
-    mask = mask << 1 ^ ord(char)
-	
-  return mask
+    mask = 0
+    filename = filename.split('/')[-1].upper()
+
+    for char in filename:
+        mask = mask << 1 ^ ord(char)
+
+    return mask
 
 def getSignature(gamePath):
-  return 'TCOAAL'
+    return b'TCOAAL'
+
+def find_game_folder():
+    drives = ['%s:' % d for d in string.ascii_uppercase if os.path.exists('%s:' % d)]
+    for drive in drives:
+        steam_path = os.path.join(drive, "\\", "SteamLibrary", "steamapps", "common", "The Coffin of Andy and Leyley")
+        if os.path.exists(steam_path):
+            return steam_path
+    return os.getcwd()
 
 if sys.version_info[0] < 3 or sys.version_info[1] < 2:
-  print('Python 3.2+ is required')
-  quit()
+    messagebox.showerror("Error", "Python 3.2+ is required")
+    sys.exit()
 
-gamePath=str(sys.argv[1])
-outPath=str(sys.argv[2])
+root = tk.Tk()
+root.withdraw()
 
-print('\nThank you Basil & Phoni for your work on tools for TCOAAL. For the OG tool visit: https://codeberg.org/basil/grimoire.\nMade by AlternativeOne with love.\n')
+gamePath = find_game_folder()
+outPath = os.path.join(os.getcwd(), "decrypted TCOAAL assets")
 
 if not os.path.exists(os.path.join(gamePath, 'www/img/system/e5230bf37c4fabb0')):
-  print('Game is not of 3.0.0+ version')
-  quit()
+    messagebox.showerror("Place .exe file in the game's dir", "The game folder was not found.\Place this .exe file in the game's root directory and try again.\n\nUsually it's in C:\\Program Files (x86)\\Steam\\steamapps\\common\\The Coffin of Andy and Leyley")
+    sys.exit()
 
-signature=getSignature(gamePath)
+os.makedirs(outPath, exist_ok=True)
+signature = getSignature(gamePath)
 
 decryptFolder('data')
 decryptFolder('audio/bgm')
@@ -97,9 +108,4 @@ decryptFolder('img/system')
 decryptFolder('img/tilesets')
 decryptFolder('img/titles1')
 
-if file_counter%50!=0:
-  print(str(file_counter)+' files processed')
-print('Success')
-
-
-
+messagebox.showinfo("Decryption successful!", f"{file_counter} files processed.\n\nThank you Basil & Phoni for your work on tools for TCOAAL. For the OG tool visit: https://codeberg.org/basil/grimoire.\nMade by AlternativeOne with love. https://github.com/AlternativeOne/tcoaal_decryptor\nPacked into .exe by @GreatFireDragon without Love.")
